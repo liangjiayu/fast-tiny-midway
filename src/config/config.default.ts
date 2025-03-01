594fd7ca4f49e87e4266a2457d6c1cf9cf7e26cb
LiangJiaYu <1013834609@qq.com>
2025-03-01 13:34:11 +0800

    init

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1740806677164_4665',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
