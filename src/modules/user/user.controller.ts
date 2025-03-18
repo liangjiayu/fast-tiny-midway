3cbbab36acee2031da75b8c9a51f5793c6656738
liangjiayu <1013834609@qq.com>
2025-03-18 22:51:16 +0800

    fix

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -19,7 +19,7 @@ export class UserController {
   userService: UserService;
 
   @Post('/create')
-  @ApiOperation({ summary: '创建用户,返回用户id' })
+  @ApiOperation({ summary: '创建用户,返回用户id123' })
   @ApiOkResponse({
     type: wrapResponse({ type: Number }),
   })
